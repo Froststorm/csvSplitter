b146280e08380d6a217ffb7f78c4ff19d9944fc2
Andriy <andrey.paladin@gmail.com>
2021-10-05 13:27:41 +0300

    improved splitting added messages

--- splitter.py
+++ splitter.py
@@ -1,19 +1,38 @@
 import os
+import time as ti
 
+files_list = os.listdir()
+files_str = " ".join(files_list)
+files_csv = []
+
+for f in files_list:
+	if f.split('.')[-1] == 'csv':
+		files_csv.append(f)
+
+print(f"CSV files in current directory: {' '.join(files_csv)}")
+
+start = ti.time()
 files = os.listdir()
 
-for f in files:
-	print(f)
 
-# os.path.(PATHS[0])
-# print(os.getcwd())
+def splitcsv(file):
+	csv_file = open(file, "r", encoding="utf-8").readlines()
+	filename = 1
+	files_csv_out = []
+	for i in range(len(csv_file)):
+		if i % 100000 == 0:
+			open(file.split('.')[0] + '_' + str(filename) + ".csv", "w+", encoding='utf-8').writelines(
+				csv_file[i: i + 100000])
+			filename += 1
+			files_csv_out.append(file.split('.')[0] + '_' + str(filename) + ".csv")
+	print(f"Files added to directory: {' '.join(files_csv_out)}")
 
 
-# csvfile = open("atu_atu.csv", "r", encoding="utf-8").readlines()
-# filename = 1
-# for i in range(len(csvfile)):
-#     if i % 100000 == 0:
-#         open(str(filename) + ".csv", "w+").writelines(csvfile[i : i + 1000])
-#         filename += 1
-#
-# print(os.listdir())
+for f in files_list:
+	ext = f.split('.')[-1]
+	if ext == 'csv':
+		splitcsv(f)
+
+stop = ti.time()
+
+print(f'elapsed {stop - start} seconds')
